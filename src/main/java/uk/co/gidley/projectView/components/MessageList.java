4d7be4cf4fad0ccc869afd541e605f9da614297f
Ben Gidley <ben@gidley.co.uk>
2010-07-21 21:58:53 +0100

    Added simple list of the messages

---
+++ src/main/java/uk/co/gidley/projectView/components/MessageList.java
@@ -0,0 +1,28 @@
+package uk.co.gidley.projectView.components;
+
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import uk.co.gidley.projectView.dao.StatusXmppMessage;
+import uk.co.gidley.projectView.services.PersistanceManager;
+
+import javax.jdo.Query;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA. User: ben Date: Jul 21, 2010 Time: 9:43:19 PM
+ */
+public class MessageList {
+
+	@Property
+	private String message;
+
+	@Inject
+	private PersistanceManager persistanceManager;
+
+	public List<String> getMessages() {
+		Query query = persistanceManager.getPmfInstance().getPersistenceManager().newQuery(StatusXmppMessage.class);
+
+		return (List<String>) query.execute();
+
+	}
+}
