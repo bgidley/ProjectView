b8260de2b69bec83ab03061ef9d7a7e8dd7812ee
Ben Gidley <ben@gidley.co.uk>
2010-07-21 20:08:17 +0100

    Initial Import

---
+++ src/main/java/uk/co/gidley/projectView/pages/Contact.java
@@ -0,0 +1,6 @@
+package uk.co.gidley.projectView.pages;
+
+public class Contact
+{
+
+}
---
+++ src/main/java/uk/co/gidley/projectView/pages/Index.java
@@ -0,0 +1,14 @@
+package uk.co.gidley.projectView.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application ProjectView.
+ */
+public class Index
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
