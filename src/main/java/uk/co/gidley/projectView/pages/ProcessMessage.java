1d5ee8e3e2717528dc6e9e343cfee8ea78011e7b
Ben Gidley <ben@gidley.co.uk>
2010-08-05 10:48:25 +0100

    Added processing of messages via queue

--- src/main/java/uk/co/gidley/projectView/pages/ProcessMessage.java
+++ src/main/java/uk/co/gidley/projectView/pages/ProcessMessage.java
@@ -4,6 +4,9 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.co.gidley.projectView.dao.StatusXmppMessage;
+
+import javax.jdo.PersistenceManager;
 
 
 /**
@@ -16,11 +19,20 @@ public class ProcessMessage {
 	private Request request;
 	public static final String KEY = "key";
 
-	public void onActivate(){
+	@Inject
+	private PersistenceManager persistenceManager;
+
+
+	public void onActivate() {
 
 		String key = (String) request.getParameter(KEY);
 		logger.debug("Key passed {}", key);
 
-		
+		StatusXmppMessage statusXmppMessage = (StatusXmppMessage) persistenceManager.getObjectById(
+				StatusXmppMessage.class, key);
+
+		// parse message and create appropiate objects
+
+
 	}
 }
